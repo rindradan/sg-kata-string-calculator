1359d8df16a052750f453f3cd9fa95316e963ace
rindra <rindra.randriambololona@gmail.com>
2022-03-02 14:11:44 +0100

    refactor

--- src/main/java/fr/kata/CalculatorApp.java
+++ src/main/java/fr/kata/CalculatorApp.java
@@ -1,12 +1,20 @@
 package fr.kata;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CalculatorApp {
 
     private final static String DEFAULT_DELIMITER = "[,\n]";
+    private final List<Function<List<Integer>, List<Integer>>> filters = new ArrayList<>();
+
+    public CalculatorApp() {
+        filters.add(filterNegatives());
+        filters.add(filterThousands());
+    }
 
     public int add(String input) {
         try {
@@ -33,12 +41,23 @@ public class CalculatorApp {
         return input;
     }
 
+    private Function<List<Integer>, List<Integer>> filterNegatives() {
+        return numbers -> {
+            String negatives = numbers.stream().filter(num -> num < 0).map(Object::toString).collect(Collectors.joining(","));
+            if (!negatives.isEmpty()) { throw new IllegalArgumentException("negatives not allowed: " + negatives); }
+            return numbers;
+        };
+    }
+
+    private Function<List<Integer>, List<Integer>> filterThousands() {
+        return numbers -> numbers.stream().filter(num -> num <= 1000).collect(Collectors.toList());
+    }
+
     private List<Integer> formatNumbers(String[] numbersTab) {
-        var numbers = Arrays.stream(numbersTab).map(Integer::parseInt).toList();
-
-        var negatives = numbers.stream().filter(num -> num < 0).map(Object::toString).collect(Collectors.joining(","));
-        if (!negatives.isEmpty()) { throw new IllegalArgumentException("negatives not allowed: "+ negatives); }
-
-        return numbers.stream().filter(num -> num <= 1000).collect(Collectors.toList());
+        var numbers = Arrays.stream(numbersTab).map(Integer::parseInt).collect(Collectors.toList());
+        for (Function<List<Integer>, List<Integer>> filter : filters) {
+            numbers = filter.apply(numbers);
+        }
+        return numbers;
     }
 }
